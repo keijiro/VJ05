0923e07d20fcec3bc735d1595f8ff8877dc0c082
Keijiro Takahashi <keijiro@ZX.local>
2015-06-30 21:39:31 +0900

    Initial commit.

---
+++ Assets/Furball/Furball.shader
@@ -0,0 +1,45 @@
+﻿Shader "Custom/Furball"
+{
+    Properties
+    {
+        _Color      ("Color",      Color) = (1,1,1,1)
+        _Metallic   ("Metallic",   Range(0,1)) = 0.5
+        _Smoothness ("Smoothness", Range(0,1)) = 0.5
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        
+        CGPROGRAM
+
+        #include "ClassicNoise3D.cginc"
+
+        #pragma surface surf Standard vertex:vert fullforwardshadows
+        #pragma target 3.0
+
+        struct Input
+        {
+            float dummy;
+        };
+
+        half4 _Color;
+        half _Smoothness;
+        half _Metallic;
+
+        void vert(inout appdata_full v)
+        {
+            v.vertex.xyz *= 1 + pow(cnoise(v.color + float3(_Time.y, 0, _Time.x)),3) * 2;
+        }
+
+        void surf(Input IN, inout SurfaceOutputStandard o)
+        {
+            o.Albedo = _Color.rgb;
+            o.Metallic = _Metallic;
+            o.Smoothness = _Smoothness;
+            o.Alpha = _Color.a;
+        }
+
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
---
+++ Assets/Furball/FurballRenderer.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Furball
+{
+    public class FurballRenderer : MonoBehaviour
+    {
+        Mesh _mesh;
+
+        [SerializeField]
+        Material _material;
+
+        Mesh BuildMesh(VertexCache vc)
+        {
+            var vertices = new Vector3[vc.triangles.Count * 12];
+            var colors = new Color[vc.triangles.Count * 12];
+            var offs = 0;
+            foreach (var t in vc.triangles)
+            {
+                var v1 = vc.vertices[t.i1];
+                var v2 = vc.vertices[t.i2];
+                var v3 = vc.vertices[t.i3];
+
+                var c = Vector3.Cross(v2 - v1, v3 - v1).normalized;
+                var center = new Color(c.x, c.y, c.z);
+
+                colors[offs] = center;
+                vertices[offs++] = v1;
+                colors[offs] = center;
+                vertices[offs++] = v2;
+                colors[offs] = center;
+                vertices[offs++] = v3;
+
+                colors[offs] = center;
+                vertices[offs++] = Vector3.zero;
+                colors[offs] = center;
+                vertices[offs++] = v2;
+                colors[offs] = center;
+                vertices[offs++] = v1;
+
+                colors[offs] = center;
+                vertices[offs++] = Vector3.zero;
+                colors[offs] = center;
+                vertices[offs++] = v3;
+                colors[offs] = center;
+                vertices[offs++] = v2;
+
+                colors[offs] = center;
+                vertices[offs++] = Vector3.zero;
+                colors[offs] = center;
+                vertices[offs++] = v1;
+                colors[offs] = center;
+                vertices[offs++] = v3;
+            }
+
+            var indices = new int[vertices.Length];
+            for (var i = 0; i < indices.Length; i++)
+                indices[i] = i;
+
+            var mesh = new Mesh();
+            mesh.vertices = vertices;
+            mesh.colors = colors;
+            mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+            mesh.RecalculateNormals();
+
+            return mesh;
+        }
+
+        void Start()
+        {
+            IcosphereBuilder ib = new IcosphereBuilder();
+            ib.Subdivide();
+            ib.Subdivide();
+            _mesh = BuildMesh(ib.vertexCache);
+        }
+
+        void Update()
+        {
+            Graphics.DrawMesh(_mesh, transform.position, transform.rotation, _material, 0);
+        }
+    }
+}
